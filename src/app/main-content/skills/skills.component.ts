8ce5f92f4c38967ce91236f283b881b1475bf0ff
Galina Antipin <galina.antipin@gmail.com>
2024-10-05 18:00:37 +0200

    added skills section with html and scss

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  skills = [
+    { name: 'Angular', icon: './../../../assets/img/angular-icon.png' },
+    { name: 'TypeScript', icon: './../../../assets/img/typescript-icon.png' },
+    { name: 'JavaScript', icon: './../../../assets/img/javascript-icon.png' },
+    { name: 'HTML', icon: './../../../assets/img/html-icon.png' },
+    { name: 'Scrum', icon: './../../../assets/img/scrum-icon.png' },
+    { name: 'Firebase', icon: './../../../assets/img/firebase-icon.png' },
+    { name: 'Git', icon: './../../../assets/img/git-icon.png' },
+    { name: 'CSS', icon: './../../../assets/img/css-icon.png' },
+    { name: 'Rest API', icon: './../../../assets/img/api-icon.png' },
+    { name: 'Material Design', icon: './../../../assets/img/material-design-icon.png' },
+    { name: 'Growth Mindset', icon: './../../../assets/img/study-icon.png' },
+  ];
 }
