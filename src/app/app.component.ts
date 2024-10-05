1237286bdb5f97d09cd9931236ce8de2ce99d353
Galina Antipin <galina.antipin@gmail.com>
2024-10-05 16:46:33 +0200

    renamed folder in landing-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,18 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { ContactComponent } from './main-content/contact/contact.component';
 import { ImprintComponent } from './imprint/imprint.component';
+
 import { MyWorkComponent } from './main-content/my-work/my-work.component';
 import { ReferencesComponent } from './main-content/references/references.component';
 import { SkillsComponent } from './main-content/skills/skills.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { LandingPageComponent } from './main-content/landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AboutMeComponent, HeaderComponent, FooterComponent, ContactComponent, ImprintComponent, MyWorkComponent, ReferencesComponent, SkillsComponent, PrivacyPolicyComponent, MainContentComponent],
+  imports: [CommonModule, RouterOutlet, AboutMeComponent, HeaderComponent, FooterComponent, ContactComponent, ImprintComponent, LandingPageComponent, MyWorkComponent, ReferencesComponent, SkillsComponent, PrivacyPolicyComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,11 +4,12 @@ import { SkillsComponent } from './skills/skills.component';
 import { MyWorkComponent } from './my-work/my-work.component';
 import { ReferencesComponent } from './references/references.component';
 import { ContactComponent } from './contact/contact.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AboutMeComponent, SkillsComponent,MyWorkComponent,ReferencesComponent,ContactComponent,],
+  imports: [AboutMeComponent, SkillsComponent,MyWorkComponent,ReferencesComponent,ContactComponent,LandingPageComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
