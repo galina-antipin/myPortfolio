eb246f8b9ab5ca0a6c9876881a0475ed7f4a9fc7
Galina Antipin <galina.antipin@gmail.com>
2024-10-05 15:55:20 +0200

    added about-me component html and scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,16 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { ContactComponent } from './main-content/contact/contact.component';
 import { ImprintComponent } from './imprint/imprint.component';
-import { LandingPageComponent } from './main-content/landing-page/landing-page.component';
 import { MyWorkComponent } from './main-content/my-work/my-work.component';
 import { ReferencesComponent } from './main-content/references/references.component';
 import { SkillsComponent } from './main-content/skills/skills.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AboutMeComponent, HeaderComponent, FooterComponent, ContactComponent, ImprintComponent, LandingPageComponent, MyWorkComponent, ReferencesComponent, SkillsComponent, PrivacyPolicyComponent],
+  imports: [CommonModule, RouterOutlet, AboutMeComponent, HeaderComponent, FooterComponent, ContactComponent, ImprintComponent, MyWorkComponent, ReferencesComponent, SkillsComponent, PrivacyPolicyComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [AboutMeComponent, SkillsComponent,MyWorkComponent,ReferencesComponent,ContactComponent,],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
