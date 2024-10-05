ab8e4b848c2ff5d597edfdadbbb17eabac5fa4cf
Galina Antipin <galina.antipin@gmail.com>
2024-10-05 15:03:22 +0200

    soem problems with git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AboutMeComponent } from './main-content/about-me/about-me.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ContactComponent } from './main-content/contact/contact.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { LandingPageComponent } from './main-content/landing-page/landing-page.component';
+import { MyWorkComponent } from './main-content/my-work/my-work.component';
+import { ReferencesComponent } from './main-content/references/references.component';
+import { SkillsComponent } from './main-content/skills/skills.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AboutMeComponent, HeaderComponent, FooterComponent, ContactComponent, ImprintComponent, LandingPageComponent, MyWorkComponent, ReferencesComponent, SkillsComponent, PrivacyPolicyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
