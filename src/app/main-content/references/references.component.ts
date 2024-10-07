2d82c6bc5880346e109ab1ca8c1234293f3b250a
Galina Antipin <galina.antipin@gmail.com>
2024-10-07 13:38:40 +0200

    added references component html and scss

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -9,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class ReferencesComponent {
 
+  references = [
+    {
+      name: 'Patrick Schlieker',
+      project: 'Join',
+      reference: '"Galina was a fantastic colleague. Her ambition and her independent and confident approach to tackling and overcoming challenges drove our project forward and played a significant role in delivering well-functioning results by the deadline. I would be happy to work with Galina again."'
+    },
+    {
+      name: 'Maximilian Stigler',
+      project: 'Join',
+      reference: '"Galina was always fully committed to our project. She completed her tasks very carefully and independently and had good time management. With her attention to detail, she ensured that nothing was overlooked. Even in stressful phases, she was always positive and radiated a good mood. Because of her empathy and willingness to help, she was also important for the morale of the team. Her great determination contributed significantly to the success of the project."'
+    },
+    {
+      name: 'Alice Johnson',
+      project: 'Marketing Strategy',
+      reference: 'Alice’s strategies boosted our outreach significantly, and her creativity is unmatched.'
+    }
+  ];
+
 }
