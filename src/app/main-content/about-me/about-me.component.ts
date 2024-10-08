744176db42615ccb3f67f0367142c35cfd0b3a76
Galina Antipin <galina.antipin@gmail.com>
2024-10-08 10:08:33 +0200

    edit references component

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
-
+mouseover(){}
 }
--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -11,19 +11,21 @@ export class ReferencesComponent {
 
   references = [
     {
-      name: 'Patrick Schlieker',
-      project: 'Join',
-      reference: '"Galina was a fantastic colleague. Her ambition and her independent and confident approach to tackling and overcoming challenges drove our project forward and played a significant role in delivering well-functioning results by the deadline. I would be happy to work with Galina again."'
-    },
-    {
       name: 'Maximilian Stigler',
       project: 'Join',
       reference: '"Galina was always fully committed to our project. She completed her tasks very carefully and independently and had good time management. With her attention to detail, she ensured that nothing was overlooked. Even in stressful phases, she was always positive and radiated a good mood. Because of her empathy and willingness to help, she was also important for the morale of the team. Her great determination contributed significantly to the success of the project."'
     },
+    
     {
-      name: 'Alice Johnson',
-      project: 'Marketing Strategy',
-      reference: 'Alice’s strategies boosted our outreach significantly, and her creativity is unmatched.'
+      name: 'Patrick Schlieker',
+      project: 'Join',
+      reference: '"Galina was a fantastic colleague. Her ambition and her independent and confident approach to tackling and overcoming challenges drove our project forward and played a significant role in delivering well-functioning results by the deadline. I would be happy to work with Galina again."'
+    },
+
+    {
+      name: 'Eduard Puchaew',
+      project: 'Join | Kochwelt',
+      reference: '"I had the pleasure of working with Galina on two different group projects. Her adaptability within the team and her exceptional willingness to tackle the most complex tasks made her an invaluable asset in both projects. Even outside of group work, I observed a steep learning curve in her. Particularly impressive was her object-oriented project, which she executed professionally in a very short time. Working with Galina made the group projects always extremely pleasant and smooth."'
     }
   ];
 
