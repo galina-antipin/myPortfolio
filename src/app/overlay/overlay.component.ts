88b4ce215045db034bac749b66cfcd3f2823bb48
Galina Antipin <galina.antipin@gmail.com>
2024-10-09 10:50:42 +0200

    added new logo

---
+++ src/app/overlay/overlay.component.ts
@@ -0,0 +1,20 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-overlay',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './overlay.component.html',
+//   styleUrl: './overlay.component.scss'
+// })
+// export class OverlayComponent {
+//   // public isOpen: boolean = false;
+
+//   // open() {
+//   //   this.isOpen = true;
+//   // }
+
+//   // close() {
+//   //   this.isOpen = false;
+//   // }
+// }
