287a964d1c24faa6dfded0443386e96496b5ceee
Galina Antipin <galina.antipin@gmail.com>
2024-10-10 11:38:45 +0200

    translated mywork section

--- src/app/main-content/my-work/my-work.component.ts
+++ src/app/main-content/my-work/my-work.component.ts
@@ -1,10 +1,12 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule} from '@angular/common';
+import { Component, inject  } from '@angular/core';
+import { TranslationService } from '../../translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-my-work',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './my-work.component.html',
   styleUrl: './my-work.component.scss'
 })
@@ -12,28 +14,27 @@ import { Component } from '@angular/core';
 export class MyWorkComponent {
   projects = [
     {
-      title: 'Join',
-      description: 'Task Manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      id: 'JOIN', 
       technologies: 'JavaScript | Firebase | HTML | CSS',
       image: './../../../assets/img/join-img.png',
       liveLink: 'https://join.galina-antipin.de', 
       githubLink: 'https://github.com/galina-antipin/join'
     },
     {
-      title: 'El Pollo Loco',
-      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      id: 'POLLO_LOCO',
       technologies: 'JavaScript | HTML | CSS',
       image: './../../../assets/img/game-img.png',
       liveLink: 'https://el-pollo-loco.galina-antipin.de',
       githubLink: 'https://github.com/galina-antipin/El-Pollo-Loco'
     },
     {
-      title: 'Pokédex',
-      description: 'Based on a PokéAPI a simple library that provides and catalogues Pokemon information.',
+      id: 'POKEDEX',
       technologies: 'JavaScript | HTML | CSS | API',
       image: './../../../assets/img/pokedex-img.png',
       liveLink: '#',
       githubLink: 'https://pokedex/galina-antipin/Pokedex'
     }
   ];
+
+  translate = inject(TranslationService);
 }
