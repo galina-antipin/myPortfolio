5e336d1fb1c95703d4037d99832f3fe7f893119b
Galina Antipin <galina.antipin@gmail.com>
2024-10-10 12:32:34 +0200

    translated references section

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslationService } from '../../translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss'
 })
@@ -29,4 +31,5 @@ export class ReferencesComponent {
     }
   ];
 
+  translate = inject(TranslationService);
 }
