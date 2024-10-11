92584c5d13023042f78d093be978cbee9284d517
Galina Antipin <galina.antipin@gmail.com>
2024-10-11 09:05:30 +0200

    added new responsive menu component and a burgen menu html + scss

---
+++ src/app/shared/components/responsive-menu/responsive-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-responsive-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './responsive-menu.component.html',
+  styleUrl: './responsive-menu.component.scss'
+})
+export class ResponsiveMenuComponent {
+
+}
